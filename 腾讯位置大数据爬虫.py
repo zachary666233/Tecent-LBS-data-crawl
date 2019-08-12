85e6b5da57b1835230c558b492cabe6016b786b2
zachary666233 <36531145+zachary666233@users.noreply.github.com>
2019-08-12 20:44:00 +0800

    Update 腾讯位置大数据爬虫.py

--- 腾讯位置大数据爬虫.py
+++ 腾讯位置大数据爬虫.py
@@ -17,17 +17,19 @@ headers = {
 }
 
 '''连接数据库'''
+#我存储到mysql数据库中
 db=MySQLdb.connect(host='localhost',user='root',db='六只脚',passwd='1995216')
 cursor=db.cursor()
 
 '''输入限定的数据范围，减小存储空间'''
+#在本例中，未获得北京地区数据，所以限定范围如下（原始数据的经纬度坐标形式如：11400-114.00，3800-38.00）    
 x1=11400
 x2=11800
 y1=3800
 y2=4200
 
 def get_data(count=4,rank=1):
-    '''获取url人口数据'''
+    '''获取人口数据主程序'''
     url='https://xingyun.map.qq.com/api/getXingyunPoints'
     load={'count': count, 'rank': rank}
     response=requests.post(url,data=json.dumps(load),headers=headers)
